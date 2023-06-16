8f3b7caa04c17315ca0fc9a510d268baf5f660b0
Gustavo Pinheiro <gustavopinheirojosephcosta@gmail.com>
2023-06-15 23:58:19 -0300

    registrando comandos

--- index.ts
+++ index.ts
@@ -1,34 +1,37 @@
-import { Collection, Client, Events, GatewayIntentBits } from "discord.js";
-import path from "path";
-const fs = require('fs')
+const { Client, Events, GatewayIntentBits, Collection } = require("discord.js");
+// import path from "path";
+const path = require("path");
+const fs = require("fs");
 
 // Require the necessary discord.js classes
-const token  = 'MTA4NzA2NzExMDU0NDUxNTIxMg.G2ngeG.Q1NYWOBQR-KkDHRISC9otjSl6bK08Jjz_a_9TY'
+const token = "";
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.commands = new Collection();
 
-const commandsPath = path.join(__dirname, 'commands');
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-
+const commandsPath = path.join(__dirname, "commands");
+const commandFiles = fs
+  .readdirSync(commandsPath)
+  .filter((file) => file.endsWith(".ts"));
 for (const file of commandFiles) {
-	const filePath = path.join(commandsPath, file);
-	const command = require(filePath);
-	// Set a new item in the Collection with the key as the command name and the value as the exported module
-	if ('data' in command && 'execute' in command) {
-		client.commands.set(command.data.name, command);
-	} else {
-		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-	}
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath);
+  // Set a new item in the Collection with the key as the command name and the value as the exported module
+  if ("data" in command && "execute" in command) {
+    client.commands.set(command.data.name, command);
+  } else {
+    console.log(
+      `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+    );
+  }
 }
+console.log(client.commands)
 // When the client is ready, run this code (only once)
 // We use 'c' for the event parameter to keep it separate from the already defined 'client'
-client.once(Events.ClientReady, c => {
-
-
-	console.log(`Ready! Logged in as ${c.user.tag}`);
+client.once(Events.ClientReady, (c) => {
+  console.log(`Ready! Logged in as ${c.user.tag}`);
 });
 
 // Log in to Discord with your client's token
